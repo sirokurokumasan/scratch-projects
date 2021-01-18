88a7837f9dbddf3ac012d11cad02bba24906feae
sirokurokumasan <72143123+sirokurokumasan@users.noreply.github.com>
2021-01-18 11:47:17 +0900

    Update add1.js

--- js/add1.js
+++ js/add1.js
@@ -1,18 +1,20 @@
 class Add1{
   constructor(){}
-    getInfo(){}
-      return{
-        id:'add1'
-        name:'便利系'
-        blocks[
-          {opcode:'aa',
-           blockType: Scratch.BlockType.REPORTER,
-      }
-          {
-          opcode:'event_whenflagclicked'
-          blockType: Scratch.BlockType.COMMAND,
-          text: '旗を押す'
-          }
-
-    aa(){
-          }
+  getInfo(){}
+    return{
+      id:'add1'
+      name:'便利系'
+      blocks[
+        {opcode:'aa',
+         blockType: Scratch.BlockType.REPORTER,
+    }
+        {
+        opcode:'event_whenflagclicked'
+        blockType: Scratch.BlockType.COMMAND,
+        text: '旗を押す'
+        }
+  aa(){
+  }
+}
+          
+Scratch.extensions.register(new Add1())
