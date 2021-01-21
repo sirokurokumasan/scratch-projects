63d8bf5fe3a635a53bb8a98bbc30641f75601ed3
sirokurokumasan <72143123+sirokurokumasan@users.noreply.github.com>
2021-01-21 17:22:56 +0900

    Update add1.js

--- js/add1.js
+++ js/add1.js
@@ -5,27 +5,16 @@ class Add1{
       id:'add1'
       name:'便利系'
       blocks[
-        {opcode:'and',
-         blockType: Scratch.BlockType.REPORTER,
-         text:'[A]と[B]と[C]'
-         arguments: {
-            A: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'ねこ好きよ、集まれ。'
-            },
-            B: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'にゃんにゃん♪'
-            },
-            C: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'わーい！わーい！'
-            },
-        }
+        {opcode:'and',blockType: Scratch.BlockType.REPORTER,text:'[A]と[B]と[C]'
+         arguments:{
+            A: {type: Scratch.ArgumentType.STRING,defaultValue: 'ねこ好きよ、集まれ。'},
+            B: {type: Scratch.ArgumentType.STRING,defaultValue: 'にゃんにゃん♪'},
+            C: {type: Scratch.ArgumentType.STRING,defaultValue: 'わーい！わーい！'},
+        }},
       ]
     }
   }
-  and({A,B,C}){
+  and(){
     return A B C;
   }
 }
